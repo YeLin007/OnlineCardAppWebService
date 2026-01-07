bc956e3ab2490a98cb2af7571388ec7cbe3ab694
Your Name <youremail@yourdomain.com>
2026-01-07 18:12:26 +0800

    COMMIT2

--- Server.js
+++ Server.js
@@ -12,7 +12,7 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 10205,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 0,
